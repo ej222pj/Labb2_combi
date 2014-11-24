2cb21bc4d5c4245b78baa7128ab7b919943bc988
ej222pj <ej222pj@student.lnu.se>
2014-11-24 14:50:07 +0100

    Initial Commit

---
+++ Combination/Content/View/Camera.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Combination.Content.View
+{
+    class Camera
+    {
+        private float scale;
+        private static int border = 10;
+
+        public Camera(int width, int heigth)
+        {
+            int scaleX = (width - border * 2);
+            int scaleY = (heigth - border * 2);
+
+            scale = scaleX;
+            if (scaleY < scaleX)
+            {
+                scale = scaleY;
+            }
+        }
+
+        public Rectangle scaleParticle(float xPos, float yPos, float size)
+        {
+            int vSize = (int)(size * scale);
+
+            Vector2 smokeVector = scaleVector(xPos, yPos);
+
+            return new Rectangle((int)smokeVector.X, (int)smokeVector.Y, vSize, vSize);
+        }
+
+        public Vector2 scaleVector(float xPos, float yPos)
+        {
+            float vX = (xPos * scale + border);
+            float vY = (yPos * scale + border);
+
+            return new Vector2(vX, vY);
+        }
+
+        public float Scale
+        {
+            get { return scale; }
+        }
+    }
+}
---
+++ Combination/Content/View/ExplotionSystem.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace Combination.Content.View
+{
+    class ExplotionSystem
+    {
+        private Vector2 position;
+        //private SmokeSystem smokeSystem;
+        //Add Schockwave
+        private SplitterSystem splitterSystem;
+        private ExplotionParticle explotionParicle;
+
+        public ExplotionSystem(Viewport viewPort, ContentManager content)
+        {
+            position = new Vector2(0.5f, 0.5f);
+            //smokeSystem = new SmokeSystem(viewPort, position, content);
+            //add schockwave
+            splitterSystem = new SplitterSystem(viewPort, position, content);
+            explotionParicle = new ExplotionParticle(viewPort, position, content);                
+        }
+
+        public void Update(float timeElapsed)
+        {
+            //smokeSystem.Update(timeElapsed);  
+            //Add shockwave
+            
+        }
+
+        public void Draw(SpriteBatch spriteBatch, float timeElapsed)
+        {
+            //smokeSystem.Draw(spriteBatch);
+            //add Shockwave
+            splitterSystem.Draw(spriteBatch, timeElapsed);
+            explotionParicle.Draw(spriteBatch, timeElapsed);
+        }
+    }
+}
